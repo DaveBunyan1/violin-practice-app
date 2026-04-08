d0d99f814da443f6c3dd8b2ab91a4261aa1d0053
Dave Bunyan <davebunyan1@gmail.com>
2026-04-08 15:33:55 -0400

    Started autocorrelation documentation.

--- backend/app/audio/record.py
+++ backend/app/audio/record.py
@@ -1,14 +1,20 @@
 import sounddevice as sd  # type: ignore
 
 import numpy as np
-from typing import cast, Tuple
+from typing import cast, Tuple, Any, Optional
+import queue
 
 from pitch.autocorrelation import estimate_frequency
 from pitch.notes import freq_to_note
 
 
 SAMPLE_RATE = 44100
+BUFFER_SIZE = 2048  # number of samples per chunk
+CHANNELS = 1
 AMBIENT_NOISE_THRESHOLD = 0.0001  # Background ambient noise was all less than 1e-05, when playing volume > 0.0001
+
+last_note: Optional[str] = None
+note_queue: queue.Queue[Tuple[float, str]] = queue.Queue()
 
 
 def record_violin(
@@ -33,8 +39,12 @@ def record_violin(
 
 
 def audio_callback(
-    indata: np.ndarray, frames: int, time: sd.CallbackFlags, status: sd.CallbackFlags
+    indata: np.ndarray,
+    frames: int,
+    time: Any,
+    status: sd.CallbackFlags,
 ) -> None:
+    global last_note
     if status:
         print(status)
 
@@ -42,10 +52,14 @@ def audio_callback(
 
     # Remove background noise from mic etc.
     volume = np.linalg.norm(audio_chunk) / len(audio_chunk)
+    print(f"Volume: {volume}")
 
     if volume < AMBIENT_NOISE_THRESHOLD:
         return
 
     freq = estimate_frequency(audio_chunk, SAMPLE_RATE)
     note = freq_to_note(freq)
-    print(f"{freq:.2f} Hz → {note}")
+
+    if note != last_note:
+        note_queue.put((freq, note))
+        last_note = note
